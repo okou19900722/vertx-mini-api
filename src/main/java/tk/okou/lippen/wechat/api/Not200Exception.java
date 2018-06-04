c187d633c9b6a235e3916b122dfb3c209c2b00b4
okou19900722 <ilayan19900722@gmail.com>
2018-06-04 20:38:10 +0800

    增加exception的message

--- src/main/java/tk/okou/lippen/wechat/api/Not200Exception.java
+++ src/main/java/tk/okou/lippen/wechat/api/Not200Exception.java
@@ -4,6 +4,7 @@ public class Not200Exception extends RuntimeException {
     private final int status;
 
     public Not200Exception(int status) {
+        super("http status:" + status);
         this.status = status;
     }
 
