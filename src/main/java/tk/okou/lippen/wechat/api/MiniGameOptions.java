adba22929bf33b63e4ce40f4ff587b3802452402
okou19900722 <ilayan19900722@gmail.com>
2018-06-04 20:39:04 +0800

    将WeChatOptions改为MiniGameOptions，把api的范围缩小，并把相应无关的配置删除

---
+++ src/main/java/tk/okou/lippen/wechat/api/MiniGameOptions.java
@@ -0,0 +1,26 @@
+package tk.okou.lippen.wechat.api;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.http.HttpClientOptions;
+
+import java.util.Objects;
+
+@DataObject
+public class MiniGameOptions {
+    private HttpClientOptions apiHttpsClientOptions = new HttpClientOptions().setDefaultHost("api.weixin.qq.com").setSsl(true).setTrustAll(true).setDefaultPort(443);
+
+    public HttpClientOptions getApiHttpsClientOptions() {
+        return apiHttpsClientOptions;
+    }
+
+    public MiniGameOptions setApiHttpsClientOptions(HttpClientOptions apiHttpsClientOptions) {
+        Objects.requireNonNull(apiHttpsClientOptions, "apiHttpsClientOptions cannot be null!");
+        this.apiHttpsClientOptions = apiHttpsClientOptions;
+        return this;
+    }
+
+    public MiniGameOptions setMaxPoolSize(int maxPoolSize) {
+        this.apiHttpsClientOptions.setMaxPoolSize(maxPoolSize);
+        return this;
+    }
+}
