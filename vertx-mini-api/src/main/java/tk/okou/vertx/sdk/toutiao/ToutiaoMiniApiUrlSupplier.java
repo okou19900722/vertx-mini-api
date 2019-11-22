639a25c69b1f8caf2342c6eab71f8682da1ddb01
okou19900722 <ilayan19900722@gmail.com>
2019-11-22 13:55:28 +0800

    修改头条小程序登陆接口的url。修改登陆接口不允许指定grantType，如果指定则抛异常，让无grantType的接口直接调用父类的有grantType的接口，避免因为多态的原因再次调用到头条后，抛异常

--- vertx-mini-api/src/main/java/tk/okou/vertx/sdk/toutiao/ToutiaoMiniApiUrlSupplier.java
+++ vertx-mini-api/src/main/java/tk/okou/vertx/sdk/toutiao/ToutiaoMiniApiUrlSupplier.java
@@ -6,14 +6,14 @@ import java.text.MessageFormat;
 import static tk.okou.sdk.util.MessageFormatUtils.format;
 
 public interface ToutiaoMiniApiUrlSupplier extends BaseMiniApiUrlSupplier {
-    MessageFormat CODE_2_SESSION_FORMATTER = new MessageFormat("/api/apps/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type={3}");
+    MessageFormat CODE_2_SESSION_FORMATTER = new MessageFormat("/api/apps/jscode2session?appid={0}&secret={1}&code={2}");
     MessageFormat GET_ACCESS_TOKEN = new MessageFormat("/api/apps/token?grant_type={0}&appid={1}&secret={2}");
     MessageFormat SET_USER_STORAGE = new MessageFormat("/api/apps/set_user_storage?access_token={0}&signature={1}&openid={2}&sig_method={3}");
     MessageFormat REMOVE_USER_STORAGE = new MessageFormat("/api/apps/remove_user_storage?access_token={0}&signature={1}&openid={2}&sig_method={3}");
 
     @Override
     default String getUrlOfCode2session(String appId, String secret, String jsCode, String grantType) {
-        return format(CODE_2_SESSION_FORMATTER, appId, secret, jsCode, grantType);
+        return format(CODE_2_SESSION_FORMATTER, appId, secret, jsCode);
     }
 
     @Override
--- vertx-mini-api/src/main/java/tk/okou/vertx/sdk/toutiao/impl/ToutiaoMiniGameApiImpl.java
+++ vertx-mini-api/src/main/java/tk/okou/vertx/sdk/toutiao/impl/ToutiaoMiniGameApiImpl.java
@@ -19,14 +19,14 @@ public class ToutiaoMiniGameApiImpl extends AbstractToutiaoMiniGameApi implement
 
     @Override
     public ToutiaoMiniGameApiImpl code2session(String appId, String secret, String jsCode, Handler<AsyncResult<JsonObject>> handler) {
-        super.code2session(appId, secret, jsCode, handler);
+//        super.code2session(appId, secret, jsCode, handler);
+        super.code2session(appId, secret, jsCode, null, handler);
         return this;
     }
 
     @Override
     public ToutiaoMiniGameApiImpl code2session(String appId, String secret, String jsCode, String grantType, Handler<AsyncResult<JsonObject>> handler) {
-        super.code2session(appId, secret, jsCode, grantType, handler);
-        return this;
+        throw new RuntimeException("头像小程序不需要指定grantType");
     }
 
     @Override
