2bb7a040b0f801b31d6e3332117403b9acf9ace5
okou19900722 <ilayan19900722@gmail.com>
2026-09-23 14:27:54 +0800

    增加扩展方法

---
+++ vertx-mini-api-kotlin-coroutines/src/main/kotlin/tk/okou/vertx/sdk/ext/WechatMiniGameApi.kt
@@ -0,0 +1,82 @@
+package tk.okou.vertx.sdk.ext
+
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.coroutines.awaitResult
+import tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi
+import java.time.LocalDate
+
+
+/**
+ * Suspending version of method [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi.getGameGiftList]
+ *
+ * @param accessToken
+ * @param page
+ * @param pageSize
+ * @param startDate
+ * @param endDate
+ * @return [JsonObject]
+ *
+ * NOTE: This function has been automatically generated from [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi] using Vert.x codegen.
+ */
+suspend fun WechatMiniGameApi.getGameGiftListAwait(accessToken: String, page: Int, pageSize: Int, startDate: LocalDate, endDate: LocalDate): JsonObject {
+    return awaitResult {
+        this.getGameGiftList(accessToken, page, pageSize, startDate, endDate, it)
+    }
+}
+
+/**
+ * Suspending version of method [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi.getPublisherAdPosGeneral]
+ *
+ * @param accessToken
+ * @param page
+ * @param pageSize
+ * @param startDate
+ * @param endDate
+ * @param adSlot
+ * @return [JsonObject]
+ *
+ * NOTE: This function has been automatically generated from [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi] using Vert.x codegen.
+ */
+suspend fun WechatMiniGameApi.getPublisherAdPosGeneralAwait(accessToken: String, page: Int, pageSize: Int, startDate: LocalDate, endDate: LocalDate, adSlot: String?): JsonObject {
+    return awaitResult {
+        this.getPublisherAdPosGeneral(accessToken, page, pageSize, startDate, endDate, adSlot, it)
+    }
+}
+
+/**
+ * Suspending version of method [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi.getPublisherAdUnitGeneral]
+ *
+ * @param accessToken
+ * @param page
+ * @param pageSize
+ * @param startDate
+ * @param endDate
+ * @param adSlot
+ * @param adUnitId
+ * @return [JsonObject]
+ *
+ * NOTE: This function has been automatically generated from [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi] using Vert.x codegen.
+ */
+suspend fun WechatMiniGameApi.getPublisherAdUnitGeneralAwait(accessToken: String, page: Int, pageSize: Int, startDate: LocalDate, endDate: LocalDate, adSlot: String?, adUnitId: String?): JsonObject {
+    return awaitResult {
+        this.getPublisherAdUnitGeneral(accessToken, page, pageSize, startDate, endDate, adSlot, adUnitId, it)
+    }
+}
+
+/**
+ * Suspending version of method [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi.getPublisherSettlement]
+ *
+ * @param accessToken
+ * @param page
+ * @param pageSize
+ * @param startDate
+ * @param endDate
+ * @return [JsonObject]
+ *
+ * NOTE: This function has been automatically generated from [tk.okou.vertx.sdk.tencent.wechat.WechatMiniGameApi] using Vert.x codegen.
+ */
+suspend fun WechatMiniGameApi.getPublisherSettlementAwait(accessToken: String, page: Int, pageSize: Int, startDate: LocalDate, endDate: LocalDate): JsonObject {
+    return awaitResult {
+        this.getPublisherSettlement(accessToken, page, pageSize, startDate, endDate, it)
+    }
+}
